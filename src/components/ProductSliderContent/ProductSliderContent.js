c1d2288183a43a72d8c38ab807ce5b3e8a56dd24
yangddu <wndun21@naver.com>
2022-02-04 01:58:46 +0900

    Refactor: DiscountBadge 추가 및 수정

--- src/components/ProductSliderContent/ProductSliderContent.js
+++ src/components/ProductSliderContent/ProductSliderContent.js
@@ -14,6 +14,11 @@ function ProductSliderContent({ productList, handleSelect, selectedProduct }) {
       {productList?.map(product => (
         <SwiperSlide key={product.productId}>
           <SwiperCon>
+            {product.outside ? (
+              ''
+            ) : (
+              <DiscountBadge>{product.discountRate}%</DiscountBadge>
+            )}
             <ProductSlider
               handleSelect={handleSelect}
               selectedProduct={selectedProduct}
@@ -29,9 +34,29 @@ function ProductSliderContent({ productList, handleSelect, selectedProduct }) {
 export default ProductSliderContent;
 
 const SwiperCon = styled.div`
+  position: relative;
   display: inline-flex;
   justify-content: center;
   width: fit-content;
   height: fit-content;
   margin: 28px 0px;
+`;
+
+const DiscountBadge = styled.div`
+  position: absolute;
+  top: 0;
+  right: 5px;
+  background-image: url(//cdn.ggumim.co.kr/storage/20211117191419RW6JS6bjRm.png);
+  width: 24px;
+  height: 28px;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  font-size: 11px;
+  font-weight: bold;
+  line-height: 25px;
+  color: white;
+  text-align: center;
+  padding-left: 1px;
+  z-index: 100;
 `;
